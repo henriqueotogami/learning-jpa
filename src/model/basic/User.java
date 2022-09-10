b4afa4a4c449199b85081fc9764402babd97e375
HenriqueMAP <henrique.map@outlook.com>
2022-09-10 14:16:38 -0300

    Creating the User Entity.

---
+++ src/model/basic/User.java
@@ -0,0 +1,34 @@
+package model.basic;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String name;
+    
+    private String email;
+
+    public User() { }
+
+    public User(final String name, final String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(final Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(final String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(final String email) { this.email = email; }
+}
