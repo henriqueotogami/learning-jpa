f1ae6661407343a02aed345521945da528f2923b
HenriqueMAP <henrique.map@outlook.com>
2022-09-21 21:41:12 -0300

    Setup and connection to the MySQL database working through JPA

---
+++ src/main/java/model/basic/User.java
@@ -0,0 +1,43 @@
+package model.basic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+//@Table(name = "people")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "person_code", nullable = false)
+    private Long id;
+
+//    @Column(name = "person_name", nullable = false)
+    private String name;
+
+    private String email;
+
+    public User() { }
+
+    public User(final String name, final String email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(final Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(final String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(final String email) { this.email = email; }
+}
