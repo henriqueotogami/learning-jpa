a1cbc6ad23dc1795380a131ad8815ca36edc8eb3
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-11-09 20:47:39 -0300

    Implementation of unit tests.
    Query class for users in the database. Test objective: Number of users greater than zero in both query methods.

--- src/test/java/model/basic/query/TestGetUsers.java
+++ src/test/java/model/basic/query/TestGetUsers.java
@@ -2,12 +2,19 @@ package model.basic.query;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import junit.framework.TestCase;
 import model.basic.User;
 import model.database.jpa.PersistenceEntityManager;
 
 import java.util.List;
 
-public class TestGetUsers {
+public class TestGetUsers extends TestCase {
+
+    private static boolean isSuccessQueryOne = false;
+    private static boolean isSuccessQueryTwo = false;
+
+    private static final String DOTTED_LINE = "------------------------------------------------------------";
+    private static final char NEW_LINE = '\n';
 
     public static void main(String[] args) {
 
@@ -16,14 +23,23 @@ public class TestGetUsers {
         createEntityConnection.createConnection();
 
         executeQueryOne(createEntityConnection.getConnectionDatabase());
-        System.out.println("---------------------------------------");
-        System.out.println("---------------------------------------");
+        System.out.println(DOTTED_LINE + NEW_LINE);
+        System.out.println(DOTTED_LINE);
         executeQueryTwo(createEntityConnection.getConnectionDatabase());
+
+        assertEquals(isSuccessQueryOne, isSuccessQueryTwo);
+        System.out.println(NEW_LINE + DOTTED_LINE);
+        System.out.println("TestGetUsers: User query performed SUCCESSFULLY.");
+        System.out.println(DOTTED_LINE + NEW_LINE);
 
         createEntityConnection.closeConnection();
         System.out.println("TestGetUsers: END");
     }
 
+    /**
+     *
+     * @param databaseConnected
+     */
     private static void executeQueryOne(final EntityManager databaseConnected) {
         System.out.println("TestGetUsers - executeQueryOne(): BEGIN");
         final String javaPersistenceQueryLanguage = "SELECT user FROM User user";
@@ -35,9 +51,14 @@ public class TestGetUsers {
             System.out.println("User: " + user.getName());
             System.out.println("ID: " + user.getId());
         }
+        isSuccessQueryOne = (answerQueryUsers.isEmpty() != false);
         System.out.println("TestGetUsers - executeQueryOne(): END");
     }
 
+    /**
+     *
+     * @param databaseConnected
+     */
     private static void executeQueryTwo(final EntityManager databaseConnected) {
         final String thisClassName = TestGetUsers.class.getName();
         final String thisMethodName = TestGetUsers.class.getDeclaredMethods()[2].getName();
@@ -53,6 +74,7 @@ public class TestGetUsers {
             System.out.println("User: " + user.getName());
             System.out.println("ID: " + user.getId());
         }
+        isSuccessQueryTwo = (answerQueryUsers.isEmpty() != false);
         System.out.println(thisClassName + " - " + thisMethodName + "(): END");
     }
 }
