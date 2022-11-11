d4149d06d875dc1d7785ac6ec6bb93ea0d80c01b
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-11-10 23:36:01 -0300

    feat: Unit Tests - Insert New User into Database.
    Using the Persistence Entity Manager to insert a new user into the database.

--- src/test/java/model/basic/insert/TestNewUser.java
+++ src/test/java/model/basic/insert/TestNewUser.java
@@ -1,11 +1,20 @@
 package model.basic.insert;
 
+import junit.framework.TestCase;
 import model.basic.User;
 import model.database.jpa.PersistenceEntityManager;
 
-public class TestNewUser {
+public class TestNewUser extends TestCase {
+
+    private static boolean isSuccessInsertNewUser = false;
 
     public static void main(String[] args) {
+
+        startTest();
+
+    }
+
+    private static void startTest() {
         System.out.println("TestNewUser: BEGIN");
         try {
             PersistenceEntityManager createEntityConnection = new PersistenceEntityManager();
@@ -18,13 +27,17 @@ public class TestNewUser {
             createEntityConnection.getConnectionDatabase().persist(newUser);
             createEntityConnection.getConnectionDatabase().getTransaction().commit();
 
-            System.out.println("New User ID: " + newUser.getId());
+            System.out.println("TestNewUser - New User ID: " + newUser.getId());
             createEntityConnection.closeConnection();
+
+            isSuccessInsertNewUser = true;
+
         } catch (Exception exception) {
             exception.printStackTrace();
         } finally {
+            assertTrue(isSuccessInsertNewUser);
+            System.out.println("TestNewUser: Insertion of a new user performed successfully.");
             System.out.println("TestNewUser: END");
         }
-
     }
 }
