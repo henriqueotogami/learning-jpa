c9e50a2450f87f63453c631191e15f8a6ac0f0bc
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-11-10 23:52:11 -0300

    refactor: Unit Tests - Query Registered Products in the Database.
    Using the Persistence Entity Manager to query a user registered in the database.

--- src/test/java/model/basic/query/TestGetUser.java
+++ src/test/java/model/basic/query/TestGetUser.java
@@ -1,19 +1,32 @@
 package model.basic.query;
 
+import junit.framework.TestCase;
 import model.basic.User;
 import model.database.jpa.PersistenceEntityManager;
 
-public class TestGetUser {
+public class TestGetUser extends TestCase {
+
+    private static boolean isSuccessGetUser = false;
 
     public static void main(String[] args) {
+        startTest();
+    }
+
+    private static void startTest() {
         System.out.println("TestGetUser: BEGIN");
         PersistenceEntityManager createEntityConnection = new PersistenceEntityManager();
-        createEntityConnection.createConnection();
-
-        User user = createEntityConnection.getConnectionDatabase().find(User.class, 1L);
-        System.out.println("User found: " + user.getName());
-
-        createEntityConnection.closeConnection();
-        System.out.println("TestGetUser: END");
+        try {
+            createEntityConnection.createConnection();
+            User user = createEntityConnection.getConnectionDatabase().find(User.class, 1L);
+            System.out.println("TestGetUser - User found: " + user.getName());
+            isSuccessGetUser = (user.getName().isEmpty() == false);
+        } catch (final Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            assertTrue(isSuccessGetUser);
+            createEntityConnection.closeConnection();
+            System.out.println("TestGetUser: New user query performed successfully.");
+            System.out.println("TestGetUser: END");
+        }
     }
 }
