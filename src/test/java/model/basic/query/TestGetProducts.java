2e56451394a5eda9b2ab6993c6450e99b4a45345
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-11-10 23:49:58 -0300

    refactor: Unit Tests - Query Registered Products in the Database.
    Using the Database Access Object to query all products registered in the database.

--- src/test/java/model/basic/query/TestGetProducts.java
+++ src/test/java/model/basic/query/TestGetProducts.java
@@ -1,29 +1,45 @@
 package model.basic.query;
 
 import infrastructure.ProductDAO;
+import junit.framework.TestCase;
 import model.basic.Product;
 
 import java.util.List;
 import java.util.function.BinaryOperator;
 
-public class TestGetProducts {
+public class TestGetProducts extends TestCase {
+
+    private static boolean isSuccessGetProducts = false;
 
     public static void main(String[] args) {
 
-        final ProductDAO productDAO = new ProductDAO();
-        final List<Product> allProducts = productDAO.getAllStorageItems();
+        startTest();
+    }
 
-        for(Product product : allProducts) {
-            System.out.println("ID: " + product.getId());
-            System.out.println("Name: " + product.getName());
+    private static void startTest() {
+        System.out.println("TestGetProducts: BEGIN");
+        try {
+            final ProductDAO productDAO = new ProductDAO();
+            final List<Product> allProducts = productDAO.getAllStorageItems();
+
+            for (Product product : allProducts) {
+                System.out.println("TestGetProducts - ID: " + product.getId());
+                System.out.println("TestGetProducts - Name: " + product.getName());
+            }
+
+            final BinaryOperator<Double> sumFunction = ((totalPrice, singlePrice) -> totalPrice + singlePrice);
+            final Double minimumPrice = 0.0D;
+            final double totalProductsPrice =
+                    allProducts.stream().map(product -> product.getPrice()).reduce(minimumPrice, sumFunction).doubleValue();
+
+            System.out.println("TestGetProducts - Total products price: R$ " + totalProductsPrice);
+            isSuccessGetProducts = true;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            assertTrue(isSuccessGetProducts);
+            System.out.println("TestGetProducts: Product query performed successfully.");
+            System.out.println("TestGetProducts: END");
         }
-
-
-        final BinaryOperator<Double> sumFunction = ((totalPrice, singlePrice) -> totalPrice + singlePrice);
-        final Double minimumPrice = 0.0D;
-        final double totalProductsPrice =
-                allProducts.stream().map(product -> product.getPrice()).reduce(minimumPrice, sumFunction).doubleValue();
-
-        System.out.println("Total products price: R$ " + totalProductsPrice);
     }
 }
