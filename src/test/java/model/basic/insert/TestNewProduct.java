4ecc3b3ccf7f0f487b36d0883738762946057861
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-11-10 23:12:42 -0300

    feat: Unit Tests - Insert Product into Database
    Using the Object Access Data infrastructure and the Product Entity to insert a new product into the database.

--- src/test/java/model/basic/insert/TestNewProduct.java
+++ src/test/java/model/basic/insert/TestNewProduct.java
@@ -2,21 +2,36 @@ package model.basic.insert;
 
 import infrastructure.DataAccessObject;
 
+import junit.framework.TestCase;
 import model.basic.Product;
 
-public class TestNewProduct {
+public class TestNewProduct extends TestCase {
+
+    private static boolean isSuccessInsertNewProduct = false;
 
     public static void main(String[] args) {
 
-        Product newProduct = new Product("Pen", 7.45);
+        startTest();
+    }
 
+    private static void startTest() {
+        System.out.println("TestNewProduct: BEGIN");
         final DataAccessObject<Product> dataAccessObject = new DataAccessObject<Product>(Product.class);
-
-        dataAccessObject.openTransactionalDAO();
-        dataAccessObject.includeTransactionalDAO(newProduct);
-        dataAccessObject.closeTransactionalDAO();
-        dataAccessObject.closeConnectionDatabase();
-
-        System.out.println("Product ID: " + newProduct.getId());
+        try {
+            final Product newProduct = new Product("Pen", 7.45);
+            dataAccessObject.openTransactionalDAO();
+            dataAccessObject.includeTransactionalDAO(newProduct);
+            isSuccessInsertNewProduct = (dataAccessObject.getAllStorageItems().size() > 0);
+            System.out.println("TestNewProduct - Added product - Name: " + newProduct.getName());
+            System.out.println("TestNewProduct - Added product - ID: " + newProduct.getId());
+        } catch ( final Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            assert(isSuccessInsertNewProduct);
+            dataAccessObject.closeTransactionalDAO();
+            dataAccessObject.closeConnectionDatabase();
+            System.out.println("TestNewProduct: Product insertion performed successfully.");
+            System.out.println("TestNewProduct: END");
+        }
     }
 }
