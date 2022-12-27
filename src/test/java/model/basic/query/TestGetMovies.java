1a7e46c4ec50c1fb457444f1a9f57d0cd09e4dd2
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-12-27 20:55:33 -0300

    test: Lesson353 - Unit test for checking stored movies, using custom database queries.

--- src/test/java/model/basic/query/TestGetMovies.java
+++ src/test/java/model/basic/query/TestGetMovies.java
@@ -1,2 +1,37 @@
-package model.basic.query;public class TestGetMovies {
+package model.basic.query;
+
+import infrastructure.DataAccessObject;
+import junit.framework.TestCase;
+import model.manytomany.Movie;
+
+import java.util.List;
+
+public class TestGetMovies extends TestCase {
+
+    private static boolean isSuccessGetMovies = false;
+
+    public static void main(String[] args) { startTest(); }
+
+    private static void startTest() {
+        System.out.println("TestGetMovies: BEGIN");
+        try {
+            final DataAccessObject<Movie> dataAccessMovie = new DataAccessObject<Movie>(Movie.class);
+            try {
+                final List<Movie> moviesList =
+                        dataAccessMovie.query("getMoviesRatingHigherThan", "rating", 8.5);
+                for(Movie movie: moviesList){ System.out.println("TestGetMovies - Movie name: " + movie.getName()); }
+                isSuccessGetMovies = (moviesList.size() > 0);
+            } catch (final Exception queryException) {
+                queryException.printStackTrace();
+                throw new RuntimeException(queryException);
+            } finally {
+                assertTrue(isSuccessGetMovies);
+                dataAccessMovie.closeConnectionDatabase();
+                System.out.println("TestGetMovies: END");
+            }
+        } catch (final Exception connectionException) {
+            connectionException.printStackTrace();
+            throw new RuntimeException(connectionException);
+        }
+    }
 }
