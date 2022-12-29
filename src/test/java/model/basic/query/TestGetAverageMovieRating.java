d4028b6c73278269ad695151fee14817f6382920
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-12-29 00:38:46 -0300

    test: Lesson 354 - Unitary Testing of Movie Grade Average Class using Native JPA Custom Query.

---
+++ src/test/java/model/basic/query/TestGetAverageMovieRating.java
@@ -0,0 +1,37 @@
+package model.basic.query;
+
+import infrastructure.DataAccessObject;
+import junit.framework.TestCase;
+import model.manytomany.MovieRating;
+
+public class TestGetAverageMovieRating extends TestCase {
+
+    private static boolean isSuccessGetAverageMovieRating = false;
+
+    public static void main(String[] args) {
+        startTest();
+    }
+
+    private static void startTest() {
+        System.out.println("TestGetAverageMovieRating: BEGIN");
+        try {
+            DataAccessObject<MovieRating> dataAccessMovieRating = new DataAccessObject<MovieRating>(MovieRating.class);
+            try {
+                MovieRating movieRating = dataAccessMovieRating.queryFirstEntity("getMovieRating");
+                System.out.println("TestGetAverageMovieRating: movieRating.getAverageMovieRatings() " + movieRating.getAverageMovieRatings());
+                isSuccessGetAverageMovieRating = (movieRating.getAverageMovieRatings() > 0D);
+            } catch (final Exception queryException) {
+                queryException.printStackTrace();
+                throw new RuntimeException(queryException);
+            } finally {
+                assertTrue(isSuccessGetAverageMovieRating);
+                dataAccessMovieRating.closeConnectionDatabase();
+                System.out.println("TestGetAverageMovieRating: END");
+            }
+        } catch (final Exception connectionException) {
+            connectionException.printStackTrace();
+            throw new RuntimeException(connectionException);
+        }
+    }
+
+}
