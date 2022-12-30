c90d9dd513a95ba7a2a79365c150950c42d11693
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-12-30 00:42:49 -0300

    feat: Lesson 356/357 - Implementation of the Entity Table Per Class Inheritance type.

--- src/main/java/model/inheritance/tableperclass/ScholarshipStudent.java
+++ src/main/java/model/inheritance/tableperclass/ScholarshipStudent.java
@@ -1,10 +1,18 @@
-package model.inheritance.typeone;
+package model.inheritance.tableperclass;
 
 import javax.persistence.Entity;
 
 @Entity
 public class ScholarshipStudent extends Student{
 
+    private double scholarshipCost;
 
-    private double 
+    public ScholarshipStudent() { }
+
+    public ScholarshipStudent(final Long registrationNumber, final String name, final double scholarshipCost) {
+        super(registrationNumber, name);
+        this.scholarshipCost = scholarshipCost;
+    }
+
+    public double getScholarshipCost() { return scholarshipCost; }
 }
--- src/main/java/model/inheritance/tableperclass/Student.java
+++ src/main/java/model/inheritance/tableperclass/Student.java
@@ -1,13 +1,28 @@
-package model.inheritance.typeone;
+package model.inheritance.tableperclass;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Student {
 
     @Id
     private Long registrationNumber;
 
     private String name;
+
+    public Student() { }
+
+    public Student(Long registrationNumber, String name) {
+        super();
+        this.registrationNumber = registrationNumber;
+        this.name = name;
+    }
+
+    public Long getRegistrationNumber() { return registrationNumber; }
+
+    public String getName() { return name; }
 }
