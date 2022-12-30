10d6e299fb6b21d9ea4bbf2618b58c4a192f3776
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2022-12-30 00:44:48 -0300

    test: Lesson 356/357 - Unit Test of the Entity Table Per Class Inheritance type.

--- src/test/java/model/inheritance/tableperclass/TestNewStudent.java
+++ src/test/java/model/inheritance/tableperclass/TestNewStudent.java
@@ -1,2 +1,43 @@
-package model.inheritance.tableperclass;public class TestNemStudent {
+package model.inheritance.tableperclass;
+
+import infrastructure.DataAccessObject;
+import junit.framework.TestCase;
+import model.inheritance.singletable.ScholarshipStudent;
+import model.manytomany.MovieRating;
+
+public class TestNewStudent extends TestCase {
+
+    private static boolean isSuccessInsertNewStudent = false;
+
+    public static void main(String[] args) {
+        startTest();
+    }
+
+    private static void startTest() {
+        System.out.println("TestNewStudent: BEGIN");
+        try {
+            DataAccessObject<Student> dataAccessStudent = new DataAccessObject<>();
+            try {
+                Student student = new Student(123L, "John");
+                ScholarshipStudent scholarshipStudent = new ScholarshipStudent(456L, "Julie", 500L);
+
+                dataAccessStudent.includeAtomicTransactionalDAO(student);
+                dataAccessStudent.includeAtomicTransactionalDAO(scholarshipStudent);
+
+                dataAccessStudent.closeTransactionalDAO();
+
+                isSuccessInsertNewStudent = true;
+            } catch (final Exception queryException) {
+                queryException.printStackTrace();
+                throw new RuntimeException(queryException);
+            } finally {
+                assertTrue(isSuccessInsertNewStudent);
+                dataAccessStudent.closeConnectionDatabase();
+                System.out.println("TestNewStudent: END");
+            }
+        } catch (final Exception connectionException) {
+            connectionException.printStackTrace();
+            throw new RuntimeException(connectionException);
+        }
+    }
 }
